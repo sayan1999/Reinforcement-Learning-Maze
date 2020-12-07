b15b6c7b46f23af79f16b14ccdba1f8ae6d1f02b
sayan1999 <mr.sayan.dey@gmail.com>
2020-12-07 09:19:58 +0530

    clean code

--- train.py
+++ train.py
@@ -24,35 +24,31 @@ class Test(Enum):
     SPEED_TEST_1 = auto()
     SPEED_TEST_2 = auto()
 
+
+# configuration setup
+mazefile='mazeData/test_12_12_1.in'
+modelpath='/content/drive/My Drive/QReplayNetworkModel'
 test = Test.DEEP_Q  # which test to run
+episodesPerNet = 4
 
-maze, nets = readMaze('mazeData/test_12_12_1.in')
-games = [Maze(maze, *net) for net in nets][:1]
+def canload(modelpath):
+    if (True if os.path.isfile(modelpath) else False):
+        print('Loading old model for retrain')
+        return True
+    print('Initializing new model')
+    return False
 
-modelpath="/content/drive/My Drive/QReplayNetworkModel"
-load = True if os.path.isfile(modelpath) else False
+load = canload(modelpath)
+maze, nets = readMaze(mazefile)
+games = [Maze(maze, *net) for net in nets]
 
+# training
 for netID, game in enumerate(games):
 
-    # train using a neural network with experience replay (also saves the resulting model)
     if test == Test.DEEP_Q:
+        ## uncomment to turn GUI off
         # game.render(Render.TRAINING)
         model = models.QReplayNetworkModel(game, name=modelpath, load=load)
-        h, w, _, _ = model.train(discount=0.80, exploration_rate=0.10, episodes=4, max_memory=maze.size * 4,
+        h, w, _, _ = model.train(discount=0.80, exploration_rate=0.10, episodes=episodesPerNet, max_memory=maze.size * 4,
                                 stop_at_convergence=True)
         print(f'Net ID: {netID} completed')
-
-# draw graphs showing development of win rate and cumulative rewards
-try:
-    h  # force a NameError exception if h does not exist, and thus don't try to show win rate and cumulative reward
-    fig, (ax1, ax2) = plt.subplots(2, 1, tight_layout=True)
-    fig.canvas.set_window_title(model.name)
-    ax1.plot(*zip(*w))
-    ax1.set_xlabel("episode")
-    ax1.set_ylabel("win rate")
-    ax2.plot(h)
-    ax2.set_xlabel("episode")
-    ax2.set_ylabel("cumulative reward")
-    plt.show()
-except NameError:
-    pass
