0678c1eb70158dc55324d4ac41c1d24b28b6fbcb
sayan1999 <mr.sayan.dey@gmail.com>
2020-12-08 22:06:26 +0530

    single blockade branch

--- retrain.py
+++ retrain.py
@@ -1,7 +1,7 @@
 import subprocess
 
 c=0
-for i in range(40):
+for i in range(1):
     subprocess.call(['python3', 'train.py'])
     c+=4
     print(f'---------------------------------------{c} episodes over---------------------------------------')
